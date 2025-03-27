c0d102d61b89d1aaeceb505f772d955cb0be36f2
Gabriel Gambôa <gabriel_gamboa10@hotmail.com>
2025-03-26 21:28:26 -0300

    feat: get author by id query

--- src/authors/infra/graphql/resolvers/authors.resolver.ts
+++ src/authors/infra/graphql/resolvers/authors.resolver.ts
@@ -3,12 +3,15 @@ import { Author, SearchAuthorsResult } from '../models';
 import { ListAuthorsUsecase } from '@/authors/domain/usecases/list-authors-usecase';
 import { CreateAuthorUseCase } from '@/authors/domain/usecases/create-author-usecase';
 import { CreateAuthorInput, SearchParamsArgs } from '../types';
+import { GetAuthorUseCase } from '@/authors/domain/usecases/get-author-usecase';
+import { AuthorIdArgs } from '../types/args/author-id.args';
 
 @Resolver(() => Author)
 export class AuthorsResolver {
   constructor(
     private readonly listAuthorsUseCase: ListAuthorsUsecase.Usecase,
     private readonly createAuthorUseCase: CreateAuthorUseCase.UseCase,
+    private readonly getAuthorUseCase: GetAuthorUseCase.UseCase,
   ) {}
 
   @Query(() => SearchAuthorsResult)
@@ -16,6 +19,13 @@ export class AuthorsResolver {
     @Args() args: SearchParamsArgs,
   ): Promise<ListAuthorsUsecase.Output> {
     return this.listAuthorsUseCase.execute(args);
+  }
+
+  @Query(() => Author)
+  async getAuthorById(
+    @Args() data: AuthorIdArgs,
+  ): Promise<GetAuthorUseCase.Output> {
+    return this.getAuthorUseCase.execute(data);
   }
 
   @Mutation(() => Author)
